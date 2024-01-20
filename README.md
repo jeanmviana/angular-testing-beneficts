e61e18f095aefbf591aca48017bef72efd9a096c
jeanmviana <jeanviana.dev@gmail.com>
2024-01-20 09:43:52 -0300

    adicionando todos os arquivos

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-Projeto de Site com Teste para consulta pública de requisitos para concessão de Benefício Assistencial - BPC - LOAS.
-
-ACESSE https://jeanmviana.github.io/project-testing/
+em construção...
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/project-buzzfeed'),
+      dir: require('path').join(__dirname, './coverage/teste-magica'),
       subdir: '.',
       reporters: [
         { type: 'html' },
